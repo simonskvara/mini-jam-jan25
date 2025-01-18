caa45c74ede44a33eadc0c65f1f7988bf58aa234
Simon SkvaraNB <simon.skvara@ue-germany.de>
2025-01-18 17:51:29 +0100

    [ADD] Hydrogen completed, water only prefab
    adding hydrogen, water is only a prefab right now

---
+++ Assets/Scripts/Elements/Hydrogen.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class Hydrogen : MonoBehaviour
+{
+    public GameObject water;
+    
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Oxygen"))
+        {
+            CreateWater((other.gameObject.transform.position + gameObject.transform.position) / 2f);
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+        }
+    }
+
+    private void CreateWater(Vector2 position)
+    {
+        GameObject spawnedWater = Instantiate(water, position, Quaternion.identity);
+    }
+}
