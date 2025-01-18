72e9b13e8fee857a336f0ad817885f2c9d135ec5
Simon SkvaraNB <simon.skvara@ue-germany.de>
2025-01-18 18:40:56 +0100

    [ADD, CHANGE] Added pause menu, changed calcium visuals
    Added pause menu, changed calcium visuals

---
+++ Assets/Scripts/PauseMenu/PauseMenu.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused;
+
+    public GameObject pauseMenu;
+
+    private void Start()
+    {
+        pauseMenu.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+
+    private void Pause()
+    {
+        pauseMenu.SetActive(true);
+        GameIsPaused = true;
+        Time.timeScale = 0f;
+    }
+
+    private void Resume()
+    {
+        pauseMenu.SetActive(false);
+        GameIsPaused = false;
+        Time.timeScale = 1f;
+    }
+
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
